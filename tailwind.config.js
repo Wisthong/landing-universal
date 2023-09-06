dead418eaab1bb5b96661d68ba1dd5f221b4a5fc
Wisthong Martinez <wisthongd@gmail.com>
2023-09-06 14:43:17 -0500

    Cambios recomendados por el diseñador

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        passion: ["Passion One", "cursive"],
+      },
+      colors: {
+        title: "#213665",
+        rosado: "#FFD1BD",
+        azulclarito: "#B6EEFF",
+      },
+    },
   },
   plugins: [],
 };
