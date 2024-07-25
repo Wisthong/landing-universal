7ef9d43275f340164b072b25a7227243fc6d7431
Wisthong David Martinez Castro <wisthongd@gmail.com>
2024-07-25 08:19:21 -0500

    Corregi ortografia del productos y añadi mediaquerys a los correos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  exports: [],
-  bootstrap: [AppComponent],
-})
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+@NgModule({ declarations: [AppComponent],
+    exports: [],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
